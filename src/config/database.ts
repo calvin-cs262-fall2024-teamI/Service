efe7a00568cd84084b1238783346ba2a39a3826e
jetoncesa <cesajjeton@gmail.com>
2024-11-18 17:31:35 -0500

    trying to get the thing to connect

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,17 +3,27 @@ import { Sequelize } from "sequelize";
 
 dotenv.config();
 
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
+
 const sequelize = new Sequelize({
   dialect: "postgres",
   database: process.env.DB_NAME,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
-  port: 5432,
-  // logging: false,
+  port: parseInt(process.env.DB_PORT!),
   logging: process.env.SQL_LOGGING === "true" ? console.log : false,
   dialectOptions: {
-    ssl: { rejectUnauthorized: false },
+    ssl: true,
+    rejectUnauthorized: false,
+  },
+  retry: {
+    max: 3, // Retry max 3 times
+    match: [/SequelizeConnectionRefusedError/, /ECONNREFUSED/], // Match the connection refused errors
   },
 });
 
