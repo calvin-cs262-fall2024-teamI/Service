831fb65b2ba63fb56dbba3c4616dba914720642c
jetoncesa <cesajjeton@gmail.com>
2024-11-18 18:40:30 -0500

    removed testing script from package.json and added routes to readme file, and the server is deployed

--- README.md
+++ README.md
@@ -8,6 +8,10 @@ This is the backend service for [SwoleMate](https://github.com/calvin-cs262-fall
 
 [Sample Data](./scripts/sample_data.ts)
 
+### Default Domain
+
+[swolemate-service.azurewebsites.net](swolemate-service.azurewebsites.net)
+
 ### Setup
 
 1. Install dependencies `npm install`
--- src/config/database.ts
+++ src/config/database.ts
@@ -3,12 +3,6 @@ import { Sequelize } from "sequelize";
 
 dotenv.config();
 
-console.log("DB_NAME:", process.env.DB_NAME);
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
-console.log("DB_HOST:", process.env.DB_HOST);
-console.log("DB_PORT:", process.env.DB_PORT);
-
 const sequelize = new Sequelize({
   dialect: "postgres",
   database: process.env.DB_NAME,
@@ -16,25 +10,12 @@ const sequelize = new Sequelize({
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT!),
+  // logging: false,
   logging: process.env.SQL_LOGGING === "true" ? console.log : false,
   dialectOptions: {
     ssl: true,
     rejectUnauthorized: false,
   },
-  retry: {
-    max: 3, // Retry max 3 times
-    match: [/SequelizeConnectionRefusedError/, /ECONNREFUSED/], // Match the connection refused errors
-  },
 });
-// Test the database connection
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log(
-      "Connection to the database has been established successfully."
-    );
-  })
-  .catch(error => {
-    console.error("Unable to connect to the database:", error);
-  });
+
 export { sequelize };
