a7bdce965a63adb007f3ce27e561a58cd417aaab
jetoncesa <cesajjeton@gmail.com>
2024-11-21 19:03:03 -0500

    fixed build error

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,10 +1,10 @@
-//import { Request,NextFunction } from "express";
+import { Request } from "express";
 import { Response } from "express";
 import { ApiResponse } from "@/utils/responseWrapper";
 
 export const errorHandler = (
   err: Error,
-  // req: Request,
+  req: Request,
   res: Response,
   // next: NextFunction,
 ) => {
